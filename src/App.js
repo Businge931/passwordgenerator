165aeb7fb3e199a0fa37cfbf020f79a08b9ba3c0
Businge931 <jimmybusinge9@gmail.com>
2022-10-05 23:13:37 +0300

    Functions for generating password

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classes from "./App.module.css";
+import Button from "./components/Button/Button";
+import CharacterLength from "./components/CharacterLength/CharacterLength";
+import Checkboxes from "./components/Checkboxes/Checkboxes";
+import Password from "./components/Password/Password";
+import Strength from "./components/Strength/Strength";
 
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [password, setPassword] = useState();
+  const [passwordLength, setPasswordLength] = useState(5);
+  const [strengthLabel, setStrengthLabel] = useState("");
+  const [strengthColors, setStrengthColors] = useState(false);
+
+  const generateRandomString = (myLength) => {
+    const chars =
+      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890`!@#$%^&*()_+-={};':\\|,.<>/?~";
+    const randomArray = Array.from(
+      { length: myLength },
+      () => chars[Math.floor(Math.random() * chars.length)]
+    );
+    const randomString = randomArray.join("");
+    return randomString;
+  };
+  const generateRandomLetter = (myLength = 1) => {
+    const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    const randomArray = Array.from(
+      { length: myLength },
+      () => chars[Math.floor(Math.random() * chars.length)]
+    );
+    const randomString = randomArray.join("");
+    return randomString;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <h1 className={classes.mainHeading}>Password Generator</h1>
+      <Password password={password} />
+      <div className={classes.App}>
+        <CharacterLength
+          passwordLength={passwordLength}
+          setPasswordLength={setPasswordLength}
+        />
+        <Checkboxes
+          password={password}
+          setPassword={setPassword}
+          onReplaceLetter={generateRandomLetter}
+        />
+        <Strength
+          passwordLength={passwordLength}
+          strengthLabel={strengthLabel}
+          strengthColors={strengthColors}
+          password={password}
+        />
+        <Button
+          onGeneratePassword={generateRandomString}
+          onSetPassword={setPassword}
+          passwordLength={passwordLength}
+          onSetStrengthLabel={setStrengthLabel}
+          onsetStrengthColors={setStrengthColors}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
