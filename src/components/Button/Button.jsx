bea7a958bcecf806b9e93006105ad3d4baf32a1f
Businge931 <jimmybusinge9@gmail.com>
2022-10-07 14:55:51 +0300

    refactoring call logic

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,22 +2,9 @@ import React from "react";
 import { BsArrowRightShort } from "react-icons/bs";
 import classes from "./Button.module.css";
 
-const Button = ({
-  onGeneratePassword,
-  onSetPassword,
-  passwordLength,
-  onSetStrengthLabel,
-  onsetStrengthColors,
-  onChangePassword,
-}) => {
-  const generatePasswordHandler = () => {
-    const generatePassword = onGeneratePassword(passwordLength);
-    onSetPassword(generatePassword);
-    onsetStrengthColors(true);
-  };
-
+const Button = ({ handleGenerate }) => {
   return (
-    <button className={classes.button} onClick={generatePasswordHandler}>
+    <button className={classes.button} onClick={handleGenerate}>
       Generate
       <BsArrowRightShort className={classes.icon} />
     </button>
