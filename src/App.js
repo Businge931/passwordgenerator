e0e539b33a16473f7165c4e3c30b0562dab91e88
Businge931 <jimmybusinge9@gmail.com>
2022-10-07 14:55:31 +0300

    function calls

--- src/App.js
+++ src/App.js
@@ -7,60 +7,51 @@ import Password from "./components/Password/Password";
 import Strength from "./components/Strength/Strength";
 
 import { useState } from "react";
+import { generateRandomPassword } from "./utils";
 
 const App = () => {
   const [password, setPassword] = useState();
   const [passwordLength, setPasswordLength] = useState(5);
-  const [strengthLabel, setStrengthLabel] = useState("");
   const [strengthColors, setStrengthColors] = useState(false);
+  const [error, setError] = useState("");
 
-  const generateRandomString = (myLength) => {
-    const chars =
-      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890`!@#$%^&*()_+-={};':\\|,.<>/?~";
-    const randomArray = Array.from(
-      { length: myLength },
-      () => chars[Math.floor(Math.random() * chars.length)]
-    );
-    const randomString = randomArray.join("");
-    return randomString;
+  const [values, setValues] = useState({
+    hasUpperCase: true,
+    hasSymbols: true,
+    hasNumbers: true,
+    hasLowerCase: true,
+  });
+
+  const onChangesHandler = (e) => {
+    const { name, checked } = e.target;
+    setValues({ ...values, [name]: checked });
   };
-  const generateRandomLetter = (myLength = 1) => {
-    const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-    const randomArray = Array.from(
-      { length: myLength },
-      () => chars[Math.floor(Math.random() * chars.length)]
-    );
-    const randomString = randomArray.join("");
-    return randomString;
+
+  const errorMessage = <p>Can not generate password!</p>;
+
+  const handleGenerate = () => {
+    const newPassword = generateRandomPassword(values, passwordLength);
+    setPassword(newPassword);
+    setStrengthColors(true);
+    setError(errorMessage);
   };
 
   return (
     <div className={classes.root}>
       <h1 className={classes.mainHeading}>Password Generator</h1>
-      <Password password={password} />
+      <Password password={password} error={error} errorMessage={errorMessage} />
       <div className={classes.App}>
         <CharacterLength
           passwordLength={passwordLength}
           setPasswordLength={setPasswordLength}
         />
-        <Checkboxes
-          password={password}
-          setPassword={setPassword}
-          onReplaceLetter={generateRandomLetter}
-        />
+        <Checkboxes values={values} onChangesHandler={onChangesHandler} />
         <Strength
           passwordLength={passwordLength}
-          strengthLabel={strengthLabel}
           strengthColors={strengthColors}
           password={password}
         />
-        <Button
-          onGeneratePassword={generateRandomString}
-          onSetPassword={setPassword}
-          passwordLength={passwordLength}
-          onSetStrengthLabel={setStrengthLabel}
-          onsetStrengthColors={setStrengthColors}
-        />
+        <Button handleGenerate={handleGenerate} />
       </div>
     </div>
   );
